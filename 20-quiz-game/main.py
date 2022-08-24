25afb86d4913c5aa249886e96fb23066431a2e6b
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-08-24 10:24:57 +0100

    quiz game

--- 20-quiz-game/main.py
+++ 20-quiz-game/main.py
@@ -1,12 +1,15 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from art import logo
+
+print(logo)
 
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 

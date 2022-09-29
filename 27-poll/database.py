1a7d8ad809830208cd470018c6ed0e646a3e83e9
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-09-29 08:34:12 +0100

    adds get random vote

--- 27-poll/database.py
+++ 27-poll/database.py
@@ -29,6 +29,8 @@ WHERE polls.id = (
     SELECT id FROM polls ORDER BY id DESC LIMIT 1
 );"""
 
+SELECT_RANDOM_VOTE = "SELECT * FROM votes WHERE option_id = %s ORDER BY RANDOM() LIMIT 1;"
+
 # Insert
 
 INSERT_OPTION = "INSERT INTO options (option_text, poll_id) VALUES %s;"
@@ -78,7 +80,8 @@ def get_poll_and_vote_results(connection, poll_id):
 def get_random_poll_vote(connection, option_id):
     with connection:
         with connection.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_RANDOM_VOTE, (option_id,))
+            return cursor.fetchone()
 
 
 def create_poll(connection, title, owner, options):

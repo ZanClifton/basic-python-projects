37695a7231dbaf8d0c896ecf2771c7797a106323
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-09-29 09:13:29 +0100

    basic functionality complete

--- 27-poll/database.py
+++ 27-poll/database.py
@@ -31,6 +31,18 @@ WHERE polls.id = (
 
 SELECT_RANDOM_VOTE = "SELECT * FROM votes WHERE option_id = %s ORDER BY RANDOM() LIMIT 1;"
 
+SELECT_POLL_VOTE_DETAILS = """
+SELECT
+    options.id,
+    options.option_text,
+    COUNT(votes.option_id) AS vote_count,
+    COUNT(votes.option_id) / SUM(COUNT(votes.option_id)) OVER() * 100.0 AS vote_percentage
+FROM options
+LEFT JOIN votes ON options.id = votes.option_id
+WHERE options.poll_id = %s
+GROUP BY options.id;
+"""
+
 # Insert
 
 INSERT_OPTION = "INSERT INTO options (option_text, poll_id) VALUES %s;"
@@ -74,7 +86,8 @@ def get_poll_details(connection, poll_id):
 def get_poll_and_vote_results(connection, poll_id):
     with connection:
         with connection.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_POLL_VOTE_DETAILS, (poll_id,))
+            return cursor.fetchall()
 
 
 def get_random_poll_vote(connection, option_id):
